57c5a6a2b4edc075f6dcc83324efdf1810fae940
BrendaWanjiru <brendawanjiru5@gmail.com>
2024-03-14 11:21:21 +0000

    alx backend python

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 9's module.
+'''
+from typing import Iterable, List, Sequence, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''Compute length of a list in sequence
+    '''
+    return [(i, len(i)) for i in lst]
